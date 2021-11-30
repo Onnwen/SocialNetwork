0f736448475f075c9e51692b39395ce7a0ee633f
Onnwen <onnwen.cassitto@icloud.com>
2021-11-30 10:00:48 +0100

    Aggiornata classe Id

--- src/Id.java
+++ src/Id.java
@@ -1,6 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Id {
 	static public int genera() {
+		/*
+		try {
+			File generalData = new File("general-data.txt");
+			Scanner fileReader = new Scanner(generalData);
+			int idMax = fileReader.nextInt();
+			fileReader.close();
+			return idMax++;
+		} catch (FileNotFoundException e) {
+			System.out.println("Non è stato trovato il file \"general-data.txt\". Caricamento abortito.");
+			try {
+				File usersFile = new File("general-data.txt");
+				usersFile.createNewFile();
+			} catch (IOException e1) {
+				System.out.println("E' stato riscontrato un errore con la scrittura del file \"general-data.txt\". Avvio abortito.");
+			}
+		}
+		*/
 		return 1;
 	}
 }

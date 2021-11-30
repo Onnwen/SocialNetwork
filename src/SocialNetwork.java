080562c0bc818732a2ce562e2659ae8114226c21
Onnwen <onnwen.cassitto@icloud.com>
2021-11-30 09:41:46 +0100

    Corretto un bug legato al caricamento dei commenti

--- src/SocialNetwork.java
+++ src/SocialNetwork.java
@@ -17,6 +17,14 @@ public class SocialNetwork {
 		this.carica();
 	}
 	
+	public Utente[] getUtenti() {
+		return Arrays.copyOf(utenti, utentiTotali);
+	}
+	
+	public Utente getUtenteConnesso() {
+		return utenteConnesso;
+	}
+	
 	public void addUtente(Utente utente) {
 		utenti[utentiTotali] = utente;
 		utentiTotali++;
@@ -27,7 +35,18 @@ public class SocialNetwork {
 	}
 	
 	public void addCommento(Commento commento, int idUtente, int idPost) {
-		cercaUtente(idUtente).getPost(idPost).addCommento(commento);
+		getPost(idPost).addCommento(commento);
+	}
+	
+	public Post getPost(int idPost) {
+		for(Utente utente:getUtenti()) {
+			for(Post postUtente:utente.getPost()) {
+				if(postUtente.getId() == idPost) {
+					return postUtente;
+				}
+			}
+		}
+		return null;
 	}
 	
 	public Utente cercaUtente(int idUtente) {
@@ -47,10 +66,17 @@ public class SocialNetwork {
 		}
 		return null;
 	}
-
-	public boolean carica() {
-		System.out.println("Carico database...");
-		System.out.println("Leggo dati degli utenti...");
+	
+	public Utente cercaUtente(String email, String password) {
+		for(int i=0; i<utentiTotali; i++) {
+			if(utenti[i].credenziali.corrette(email, password)) {
+				return utenti[i];
+			}
+		}
+		return null;
+	}
+	
+	public void caricaUtenti() {
 		try {
 			File usersFile = new File("users.csv");
 			Scanner fileReader = new Scanner(usersFile);
@@ -74,11 +100,11 @@ public class SocialNetwork {
 				usersFile.createNewFile();
 			} catch (IOException e1) {
 				System.out.println("E' stato riscontrato un errore con la scrittura del file \"users.csv\". Avvio abortato.");
-				return false;
 			}
 		}
-		
-		System.out.println("Leggo dati dei post...");
+	}
+	
+	public void caricaPost() {
 		try {
 			File postFile = new File("post.csv");
 			Scanner fileReader = new Scanner(postFile);
@@ -102,17 +128,17 @@ public class SocialNetwork {
 				usersFile.createNewFile();
 			} catch (IOException e1) {
 				System.out.println("E' stato riscontrato un errore con la scrittura del file \"post.csv\". Avvio abortato.");
-				return false;
 			}
 		}
-		
-		System.out.println("Leggo dati dei commenti...");
+	}
+	
+	public void caricaCommenti() {
 		try {
 			File postFile = new File("comments.csv");
 			Scanner fileReader = new Scanner(postFile);
 			while(fileReader.hasNext()) {
 				String[] datiUtente = fileReader.nextLine().split(";");
-				if(datiUtente.length == 4) {
+				if(datiUtente.length == 5) {
 					int idUtente = Integer.parseInt(datiUtente[0]);
 					int idPost = Integer.parseInt(datiUtente[1]);
 					int idCommento = Integer.parseInt(datiUtente[2]);
@@ -131,13 +157,14 @@ public class SocialNetwork {
 				usersFile.createNewFile();
 			} catch (IOException e1) {
 				System.out.println("E' stato riscontrato un errore con la scrittura del file \"comments.csv\". Avvio abortato.");
-				return false;
 			}
 		}
-		
-		System.out.println("Caricamento completato con successo.\n\n\n");
-		
-		return true;
+	}
+	
+	public void carica() {
+		caricaUtenti();
+		caricaPost();
+		caricaCommenti();
 	}
 	
 	public void stampaUtente() {
@@ -149,13 +176,14 @@ public class SocialNetwork {
 	public boolean entra() {
 		int tentativi = 3;
 		
-		do{
+		do {
 			System.out.print("Inserisci email: ");
 			String emailInserita = Leggi.stringa();
 			System.out.print("Inserisci password: ");
 			String passwordInserita = Leggi.stringa();
 			
 			if(utenteEsiste(emailInserita, passwordInserita)) {
+				this.utenteConnesso = this.cercaUtente(emailInserita, passwordInserita);
 				System.out.println("Login effettuato correttamente.");
 				return true;
 			}
@@ -163,8 +191,8 @@ public class SocialNetwork {
 				tentativi--;
 				System.out.println("Nome o password errati. Tentativi rimasti: " + tentativi);
 			}
+			
 		} while(tentativi > 0);
-		
 		
 		return false;
 	}

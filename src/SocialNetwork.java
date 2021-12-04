f99f926a20b598233b68bc3644ccc775a0a89834
FilippoGardi <95119891+FilippoGardi@users.noreply.github.com>
2021-12-04 08:55:11 +0100

    Update SocialNetwork.java

--- src/SocialNetwork.java
+++ src/SocialNetwork.java
@@ -78,6 +78,39 @@ public class SocialNetwork {
 		return null;
 	}
 	
+	public void registra(){
+		Registrazione registra = new Registrazione();
+		int scelta;
+		Scanner obj = new Scanner (System.in);
+		
+		System.out.println("Scegli 2]crea account ");
+		scelta = obj.nextInt();
+		
+		switch (scelta) {
+		case 1: 
+			if (scelta == 2) {
+				System.out.println("Inserisci il nome: ");
+				String nomeUtente = obj.next();
+				System.out.print("Inserisci cognome: ");
+				String cognomeUtente = obj.next();
+				System.out.println("Inserisci eta: ");
+				int eta = obj.nextInt();
+				if(eta<10) {
+					System.out.println("Non puoi accedere al social: ");
+					break;
+				}
+				
+				registra.codiceDiRegistrazione(0);
+				
+				System.out.println("Inserisci nome del profilo: ");
+				String nomeProfilo = obj.next();
+				System.out.println("Inserisci password : ");
+				int password = obj.nextInt();
+				registra.stampa(nomeUtente, cognomeUtente, nomeProfilo);
+			}	
+		}
+	}
+	
 	public void caricaUtenti() {
 		try {
 			File usersFile = new File("users.csv");

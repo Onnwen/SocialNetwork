f73d264e386c1ed5654b9dc568f4f3c9e93a3e56
FilippoGardi <95119891+FilippoGardi@users.noreply.github.com>
2022-02-26 14:16:42 +0100

    Spostamento classe Errore java nel main

---
+++ src/Errore.java
@@ -0,0 +1,14 @@
+public class Errore extends Exception {
+     String errore = "";
+    
+    public Errore(){
+    
+    }
+    
+    public void setErrore(){
+         errore = "";
+    }
+    
+    public String getErrore(){
+        return errore;
+    }
